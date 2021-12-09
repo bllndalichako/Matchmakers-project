fe51c18530788dc23fa0b94aed2595039cff167c
Acornidehuber <91097647+Acornidehuber@users.noreply.github.com>
2021-12-09 15:49:41 -0500

    Added Adjacency Matrix implementation
    -AdjMatrix class handles its own data. 
    Should only need to call constructor and GenerateMatches(), like so:
    
    #include "AdjMatrix.h";
    using namespace std;
    
    // ====TEST-SPECIFIC CONSTANTS==== //
    string PATH = "D:/alex1/Downloads/names.csv";
    int POOLSIZE = 10;
    
    int main()
    {
    	AdjMatrix matrix(PATH, POOLSIZE);
    	map<string, string> matches = matrix.GenerateMatches();
    
    	// If you wanted to print matches:
    	for (auto it : matches)
    	{
    		          //woman				  //man
    		cout << it.first << " + " << it.second << endl;
    	}
    
    	return 0;
    }

---
+++ AdjMatrix.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <sstream>
+#include <map>
+using namespace std;
+
+class AdjMatrix
+{
+	// ========META-DATA======== //
+	int poolsize;
+	vector<int> rankingset;
+	map<string,string> matches;			//indexed by woman
+
+	// ====NAME DICTIONARIES==== //
+	vector<string> men;
+	vector<string> women;
+
+	// ===ADJACENCY MATRICES=== //
+	vector<vector<int>> womensPrefGraph;
+	vector<vector<int>> mensPrefGraph;
+
+public:
+	AdjMatrix(string& fname, int N);
+	void loadData(string& fname, int N);
+	void setPreferences();
+	bool WomanPrefersMOverM2(int w, int m, int m2) const;
+	map<string, string>& GenerateMatches();
+};
